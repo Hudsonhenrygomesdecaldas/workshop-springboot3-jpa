5e46738ae531f08a52e9d2acea2f8307da9fb791
Hudson Henry <hudson_henry2010@hotmail.com>
2023-07-03 17:11:52 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/hudsonhenry/curso/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hudsonhenry.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hudsonhenry.curso.entites.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
